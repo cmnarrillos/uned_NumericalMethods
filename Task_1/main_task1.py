211c5167ab1cad1ac7c583e23d426a42e8913a9c
cmnarrillos <cmorenonarrillos@gmail.com>
2023-10-27 00:52:50 +0200

    Version to deliver task 1

--- Task_1/main_task1.py
+++ Task_1/main_task1.py
@@ -38,12 +38,13 @@ if shooting_ref:
 
     t_init = time.time()
     y0_ref_sh, x_ref_sh, y_ref_sh = shooting_method(beam_momentum_ode, x_bc, y_bc, is_bc, 2*N_ref_sh, params, tol=1e-15)
+    t_exe_ref_sh = time.time() - t_init
 
     print(f' -> Shooting method (N=1e6)')
     print(f'      Reference Initial Condition: y0 = {y0_ref_sh} (N={N_ref_sh})')
     print(f'      Maximum momentum at the beam: M_max = {np.max(np.abs(y_ref_sh[:, 0]))}'
           f' * p_0 * L^2 (y_max = {np.max(np.abs(y_ref_sh[:, 0]))})')
-    print(f'      Computing time: {time.time() - t_init} s')
+    print(f'      Computing time: {t_exe_ref_sh} s')
 
 # Finite differences REF
 if finite_diff_ref:
@@ -58,11 +59,14 @@ if finite_diff_ref:
     y_intermediate = solve_linear_system_with_lu_decomposition(A, b)
     x_ref_fd = np.linspace(-1, 1, 2*N_ref_fd+1)
     y_ref_fd = np.concatenate(([y_bc[0]], y_intermediate, [y_bc[-1]]))
+
+    t_exe_ref_fd = time.time() - t_init
+
     print()
     print(f' -> Finite differences (N=1e4)')
     print(f'      Maximum momentum at the beam: M_max = {np.max(np.abs(y_ref_fd))} '
           f'* p_0 * L^2 (y_max = {np.max(np.abs(y_ref_fd))})')
-    print(f'      Computing time: {time.time() - t_init} s')
+    print(f'      Computing time: {t_exe_ref_fd} s')
 
 # Difference between methods
 if difference_btw_refs:
@@ -86,7 +90,7 @@ if shooting_convergence:
     params = {}
     n_list = [10, 20, 40, 80, 100, 200, 400, 800, 1000, 2000, 4000, 8000, 10000,
               20000, 40000, 80000, 100000, 200000, 400000]
-    # n_list = [10, 100, 1000]
+    # n_list = [1000, 100, 10]
 
     # Initialize lists to store results
     t_exe_list = []
@@ -115,7 +119,7 @@ if shooting_convergence:
               f' * p_0 * L^2 (y_max = {np.max(np.abs(y[:, 0]))})')
         print(f'      Computing time: {t_exe} s')
 
-        if nn < 1000:
+        if nn < 100:
             format_ = 'r+-'
         else:
             format_ = 'r-'
@@ -132,7 +136,8 @@ if shooting_convergence:
             plt.ylim(min(y_plot)*1.1 - 0.1, max(y_plot)*1.1 + 0.1)
             plt.grid(which='both')
             plt.legend()
-            plt.savefig(f'./Figures/shooting_N_{nn}_y.png')
+            plt.savefig(f'./Figures/shooting_N_{nn}_y.png', bbox_inches='tight')
+            plt.close()
 
         x_list.append(x)
         y_list.append(y[:, 0])
@@ -162,7 +167,8 @@ if shooting_convergence:
             plt.ylim(min(eps[1:])*0.1, max(eps)*10)
             plt.grid(which='both')
             plt.legend()
-            plt.savefig(f'./Figures/shooting_N_{nn}_err.png')
+            plt.savefig(f'./Figures/shooting_N_{nn}_err.png', bbox_inches='tight')
+            plt.close()
 
         x_eps_list.append(x_eps)
         eps_list.append(eps)
@@ -172,7 +178,7 @@ if shooting_convergence:
     # Plot momentum from different discretizations computed
     plt.figure()
     for ii, nn in enumerate(n_list):
-        if nn < 1000:
+        if nn < 100:
             format_ = '+-'
         else:
             format_ = '-'
@@ -183,13 +189,14 @@ if shooting_convergence:
     plt.xlim(min(x_ref_sh), max(x_ref_sh))
     plt.ylim(min(y_ref_sh[:, 0])*1.1 - 0.1, max(y_ref_sh[:, 0])*1.1 + 0.1)
     plt.grid(which='both')
-    plt.legend()
-    plt.savefig(f'./Figures/shooting_all_y.png')
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig(f'./Figures/shooting_all_y.png', bbox_inches='tight')
+    plt.close()
 
     # Plot error from different sizes computed
     plt.figure()
     for ii, nn in enumerate(n_eps_list):
-        if nn < 1000:
+        if nn < 100:
             format_ = '+-'
         else:
             format_ = '-'
@@ -199,10 +206,14 @@ if shooting_convergence:
     plt.xlim(min(x_eps_list[-1]), max(x_eps_list[-1]))
     plt.ylim(min(eps_list[-1][1:])*0.1, max(eps_list[0])*10)
     plt.grid(which='both')
-    plt.legend()
-    plt.savefig(f'./Figures/shooting_all_err.png')
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig(f'./Figures/shooting_all_err.png', bbox_inches='tight')
+    plt.close()
 
-    # Plot execution time
+    # Plot execution time vs N
+    n_list.append(N_ref_sh)
+    t_exe_list.append(t_exe_ref_sh)
+
     plt.figure()
     plt.loglog(n_list, t_exe_list, 'k+-')
     plt.xlabel('$ N $')
@@ -210,28 +221,31 @@ if shooting_convergence:
     plt.xlim(min(n_list)*0.3, max(n_list)*3)
     plt.ylim(min(t_exe_list)*0.1, max(t_exe_list)*10)
     plt.grid(which='both')
-    plt.savefig(f'./Figures/shooting_t_exe.png')
+    plt.savefig(f'./Figures/shooting_t_exe.png', bbox_inches='tight')
+    plt.close()
 
-    # Plot execution time
+    # Plot max_error vs N
     plt.figure()
     plt.loglog(n_eps_list, max_eps, 'k+-')
     plt.xlabel('$ N $')
-    plt.ylabel('$ max(error) $')
+    plt.ylabel('$ max(\Delta y) $')
     plt.xlim(min(n_eps_list)*0.3, max(n_eps_list)*3)
     plt.ylim(min(max_eps)*0.1, max(max_eps)*10)
     plt.grid(which='both')
-    plt.savefig(f'./Figures/shooting_max_err.png')
+    plt.savefig(f'./Figures/shooting_max_err.png', bbox_inches='tight')
+    plt.close()
 
     # Plot max error vs execution time
     t_exe_eps_list = [t_exe_list[n_list.index(n)] for n in n_eps_list if n in n_list]
     plt.figure()
     plt.loglog(t_exe_eps_list, max_eps, 'k+-')
     plt.xlabel('$ t_{exe}[s] $')
-    plt.ylabel('$ max(error) $')
+    plt.ylabel('$ max(\Delta y) $')
     plt.xlim(min(t_exe_eps_list)*0.3, max(t_exe_eps_list)*3)
     plt.ylim(min(max_eps)*0.1, max(max_eps)*10)
     plt.grid(which='both')
-    plt.savefig(f'./Figures/shooting_max_err_vs_t_exe.png')
+    plt.savefig(f'./Figures/shooting_max_err_vs_t_exe.png', bbox_inches='tight')
+    plt.close()
 
 
 # Study of convergence of Finite Differences method
@@ -244,8 +258,8 @@ if finite_diff_convergence:
     # Initialize params and BCs structures
     x_bc = (-1, 1)
     y_bc = (0, 0)
-    # n_list = [5, 10, 20, 40, 50, 100, 200, 400, 500, 1000, 2000, 4000, 5000]
-    n_list = [5, 10, 20, 40, 50, 100, 200, 400, 500]
+    n_list = [5, 10, 20, 40, 50, 100, 200, 400, 500, 1000, 2000, 4000, 5000]
+    # n_list = [5, 10, 20, 40, 50, 100, 200, 400, 500]
 
     # Initialize lists to store results
     t_exe_list = []
@@ -277,7 +291,7 @@ if finite_diff_convergence:
               f' * p_0 * L^2 (y_max = {np.max(np.abs(y))})')
         print(f'      Computing time: {t_exe} s')
 
-        if nn < 500:
+        if nn < 100:
             format_ = 'r+-'
         else:
             format_ = 'r-'
@@ -287,13 +301,14 @@ if finite_diff_convergence:
             plt.figure()
             plt.plot(x, y, format_, label=f'N={nn}')
             plt.plot(x_ref_fd, y_ref_fd, 'k-', label=f'REF (N={N_ref_fd})')
-            plt.xlabel('$ x=\\xi/L $')
-            plt.ylabel('$ y=M/(p_0 L^2) $')
+            plt.xlabel('$ x=\\xi/L $', fontsize=14)
+            plt.ylabel('$ y=M/(p_0 L^2) $', fontsize=14)
             plt.xlim(min(x), max(x))
             plt.ylim(min(y)*1.1 - 0.1, max(y)*1.1 + 0.1)
             plt.grid(which='both')
             plt.legend()
-            plt.savefig(f'./Figures/finite_diff_N_{nn}_y.png')
+            plt.savefig(f'./Figures/finite_diff_N_{nn}_y.png', bbox_inches='tight')
+            plt.close()
 
         x_list.append(x)
         y_list.append(y)
@@ -317,13 +332,14 @@ if finite_diff_convergence:
         if nn < N_ref_fd:
             plt.figure()
             plt.semilogy(x_eps, eps, format_, label=f'N={nn}')
-            plt.xlabel('$ x=\\xi/L $')
-            plt.ylabel('$ \\Delta y $')
+            plt.xlabel('$ x=\\xi/L $', fontsize=14)
+            plt.ylabel('$ \\Delta y $', fontsize=14)
             plt.xlim(min(x), max(x))
             plt.ylim(min(eps[1:])*0.1, max(eps)*10)
             plt.grid(which='both')
             plt.legend()
-            plt.savefig(f'./Figures/finite_diff_N_{nn}_err.png')
+            plt.savefig(f'./Figures/finite_diff_N_{nn}_err.png', bbox_inches='tight')
+            plt.close()
 
         x_eps_list.append(x_eps)
         eps_list.append(eps)
@@ -333,66 +349,74 @@ if finite_diff_convergence:
     # Plot momentum from different discretizations computed
     plt.figure()
     for ii, nn in enumerate(n_list):
-        if nn < 1000:
+        if nn < 100:
             format_ = '+-'
         else:
             format_ = '-'
         plt.plot(x_list[ii], y_list[ii], format_, label=f'N={nn}')
     plt.plot(x_ref_fd, y_ref_fd, 'k-', label=f'REF (N={N_ref_fd})')
-    plt.xlabel('$ x=\\xi/L $')
-    plt.ylabel('$ y=M/(p_0 L^2) $')
+    plt.xlabel('$ x=\\xi/L $', fontsize=14)
+    plt.ylabel('$ y=M/(p_0 L^2) $', fontsize=14)
     plt.xlim(min(x_ref_fd), max(x_ref_fd))
     plt.ylim(min(y_ref_fd)*1.1 - 0.1, max(y_ref_fd)*1.1 + 0.1)
     plt.grid(which='both')
-    plt.legend()
-    plt.savefig(f'./Figures/finite_diff_all_y.png')
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig(f'./Figures/finite_diff_all_y.png', bbox_inches='tight')
+    plt.close()
 
     # Plot error from different sizes computed
     plt.figure()
     for ii, nn in enumerate(n_eps_list):
-        if nn < 1000:
+        if nn < 100:
             format_ = '+-'
         else:
             format_ = '-'
         plt.semilogy(x_eps_list[ii], eps_list[ii], format_, label=f'N={nn}')
-    plt.xlabel('$ x=\\xi/L $')
-    plt.ylabel('$ \\Delta y $')
+    plt.xlabel('$ x=\\xi/L $', fontsize=14)
+    plt.ylabel('$ \\Delta y $', fontsize=14)
     plt.xlim(min(x_eps_list[-1]), max(x_eps_list[-1]))
     plt.ylim(min(eps_list[-1][1:])*0.1, max(eps_list[0])*10)
     plt.grid(which='both')
-    plt.legend()
-    plt.savefig(f'./Figures/finite_diff_all_err.png')
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig(f'./Figures/finite_diff_all_err.png', bbox_inches='tight')
+    plt.close()
 
     # Plot execution time vs N
+    n_list.append(N_ref_fd)
+    t_exe_list.append(t_exe_ref_fd)
+
     plt.figure()
     plt.loglog(n_list, t_exe_list, 'k+-')
-    plt.xlabel('$ N $')
-    plt.ylabel('$ t_{exe} $')
+    plt.xlabel('$ N $', fontsize=14)
+    plt.ylabel('$ t_{exe} $', fontsize=14)
     plt.xlim(min(n_list)*0.3, max(n_list)*3)
     plt.ylim(min(t_exe_list)*0.1, max(t_exe_list)*10)
     plt.grid(which='both')
-    plt.savefig(f'./Figures/finite_diff_t_exe.png')
+    plt.savefig(f'./Figures/finite_diff_t_exe.png', bbox_inches='tight')
+    plt.close()
 
     # Plot max error vs N
     plt.figure()
     plt.loglog(n_eps_list, max_eps, 'k+-')
-    plt.xlabel('$ N $')
-    plt.ylabel('$ max(error) $')
+    plt.xlabel('$ N $', fontsize=14)
+    plt.ylabel('$ max(\Delta y) $', fontsize=14)
     plt.xlim(min(n_eps_list)*0.3, max(n_eps_list)*3)
     plt.ylim(min(max_eps)*0.1, max(max_eps)*10)
     plt.grid(which='both')
-    plt.savefig(f'./Figures/finite_diff_max_err.png')
+    plt.savefig(f'./Figures/finite_diff_max_err.png', bbox_inches='tight')
+    plt.close()
 
     # Plot max error vs execution time
     t_exe_eps_list = [t_exe_list[n_list.index(n)] for n in n_eps_list if n in n_list]
     plt.figure()
     plt.loglog(t_exe_eps_list, max_eps, 'k+-')
-    plt.xlabel('$ t_{exe}[s] $')
-    plt.ylabel('$ max(error) $')
+    plt.xlabel('$ t_{exe}[s] $', fontsize=14)
+    plt.ylabel('$ max(\Delta y) $', fontsize=14)
     plt.xlim(min(t_exe_eps_list)*0.3, max(t_exe_eps_list)*3)
     plt.ylim(min(max_eps)*0.1, max(max_eps)*10)
     plt.grid(which='both')
-    plt.savefig(f'./Figures/finite_diff_max_err_vs_t_exe.png')
+    plt.savefig(f'./Figures/finite_diff_max_err_vs_t_exe.png', bbox_inches='tight')
+    plt.close()
 
 
 # Shooting method for deformation using Euler-Bernouilli beam model
@@ -427,19 +451,21 @@ if shooting_deform:
         plt.figure()
         y_plot = y[:, 0]
         plt.plot(x, y_plot, label=f'LF={lf}')
-        plt.xlabel('$ x=\\xi/L $')
-        plt.ylabel('$ y=M/(p_0 L^2) $')
+        plt.xlabel('$ x=\\xi/L $', fontsize=14)
+        plt.ylabel('$ y=M/(p_0 L^2) $', fontsize=14)
         plt.xlim(min(x), max(x))
         plt.ylim(min(y_plot)*1.1 - 0.1, max(y_plot)*1.1 + 0.1)
         plt.grid(which='both')
-        plt.savefig(f'./Figures/test_shooting_lf_{params["load_factor"]}_N_{N}_momentum.png')
+        plt.savefig(f'./Figures/test_shooting_lf_{params["load_factor"]}_N_{N}_momentum.png', bbox_inches='tight')
+        plt.close()
 
         plt.figure()
         y_plot = y[:, 2]
         plt.plot(x, y_plot, label=f'LF={lf}')
-        plt.xlabel('$ x=\\xi/L $')
-        plt.ylabel('$ y=w/L $')
+        plt.xlabel('$ x=\\xi/L $', fontsize=14)
+        plt.ylabel('$ y=w/L $', fontsize=14)
         plt.xlim(min(x), max(x))
         plt.ylim(min(y_plot)*1.1 - 0.1, max(y_plot)*1.1 + 0.1)
         plt.grid(which='both')
-        plt.savefig(f'./Figures/test_shooting_lf_{params["load_factor"]}_N_{N}_deformation.png')
+        plt.savefig(f'./Figures/test_shooting_lf_{params["load_factor"]}_N_{N}_deformation.png', bbox_inches='tight')
+        plt.close()
